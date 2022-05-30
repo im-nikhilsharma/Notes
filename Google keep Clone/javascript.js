3c454fb10dc6e6b08ff58d717fcf6703c7e09bbb
Nikhil Sharma <84122436+im-nikhilsharma@users.noreply.github.com>
2022-05-30 14:30:28 +0530

    Add files via upload

---
+++ Google keep Clone/javascript.js
@@ -0,0 +1,9 @@
+function display(){
+    document.getElementById('wc').style.visibility = "visible"
+}
+function nextbuttonid(){
+    document.getElementById('wc2').style.visibility = "visible"
+}
+function submite(){
+    document.getElementById('overmain').style.visibility = "visible"
+}
